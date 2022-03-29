1b02129630eb3ee49c3e3b22af470852c5227598
Kiera-Hegarty <kiera-hegarty@hotmail.com>
2022-03-29 16:45:47 +0100

    working get functions

---
+++ bookings-api/db.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+mongoose.connect("mongodb+srv://admin-1:root@cluster0.06yd2.mongodb.net/main?retryWrites=true&w=majority",{
+    useNewUrlParser: true,
+});
+
+
+const bookingSchema = new Schema({
+    screenId: {
+        type: Number,
+        required: true,
+    },
+    full_name: {
+        type: String,
+        required: true,
+    },
+    number_of_seats: {
+        type: Number,
+        required: true,
+    },
+    number_of_adults: {
+        type: Number,
+        requied: true,
+    },
+    number_of_children: {
+        type: Number,
+        required: true,
+    },
+    concessions: {
+        type: Array,
+        required: true,
+    },
+});
+
+module.exports = mongoose.model("Booking", bookingSchema);
---
+++ bookings-api/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const parser = require("body-parser");
+
+const app = express();
+
+app.use(parser.json());
+
+const bookingRoutes = require("./routes/bookingRoutes");
+
+app.use((req, res, next) => {
+  // eslint-disable-next-line no-console
+  console.log(req.method, req.url, new Date());
+  return next();
+});
+
+app.get("/", (request, response) => {
+  response.send("Hello!");
+});
+
+app.use("/booking", bookingRoutes);
+
+app.use("*", (req, res, next) => next({ status: 404, message: "Invalid URL" }));
+
+app.use((err, req, res) => {
+  res.status(err.status).send(err.message);
+});
+
+const server = app.listen(4494, () => {
+  // eslint-disable-next-line no-console
+  console.log("Server successfully started on port", server.address().port);
+});
+
+module.exports = server;
