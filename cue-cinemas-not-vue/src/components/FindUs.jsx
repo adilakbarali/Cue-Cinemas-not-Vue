bb9bc115a23a73fca09d950d739bdf40e0b1d327
Kiera-Hegarty <kiera-hegarty@hotmail.com>
2022-03-31 15:31:05 +0100

    adding find us page

---
+++ cue-cinemas-not-vue/src/components/FindUs.jsx
@@ -0,0 +1,64 @@
+const FindUs = () => {
+
+    
+
+  return (
+    <>
+    <h1> How to Find Us </h1>
+      <div>
+        <h2> Address: </h2>
+        <h3> 60 Provan Walk </h3>
+        <h3> Glasgow Fort </h3>
+        <h3> Glasgow </h3>
+        <h3> G34 9DL</h3>
+        <img
+          src="https://th.bing.com/th/id/R.7d63de4db90b791a09e2e62fefd39514?rik=cxsrttKLknZz8w&riu=http%3a%2f%2fphotos.cinematreasures.org%2fproduction%2fphotos%2f213486%2f1499671381%2flarge.JPG%3f1499671381&ehk=vbLm%2bdgcEY86e7FnxE13hja%2flln3q%2fjqCHD0ZghwBhA%3d&risl=&pid=ImgRaw&r=0"
+          alt = "outside vue"
+          width="350px"
+        />
+      </div>
+      <div>
+        <h2> Directions: </h2>
+        <h3> By Car: </h3>
+        <p>
+          Vue is located within Glasgow Fort which is off of Junction 10 of the
+          M8. Glasgow Fort is 5 miles East of Glasgow City Centre or 35 miles
+          West of Edinburgh.
+        </p>
+        <h3> By Bus: </h3>
+        <p>
+          There are bus stops outside and in the middle of the shopping park.
+          There are many regular bus services that run from Glasgow City Centre
+          including First bus services: 19, 38, 46, 60/60A, and Stagecoach
+          service: X19.
+        </p>
+        <h3> By Train: </h3>
+        <p>
+          The nearest train station to Easterhouse train station which is a 41
+          minute walk or 30 minute bus ride. Bus services that run from
+          Easterhouse train station are McGill’s: 310 (every 1 hour 10 minutes),
+          or walk to 10 minutes to Easterhouse where there are two bus services
+          including; Stagecoach: X19 (every 15 minutes), and First Bus: 60/60A
+          (every 15 minutes).
+        </p>
+        <h3> By Teleportation: </h3>
+        <p>
+          Currently work in progress. Please check back in 2050 for updates.
+        </p>
+        <h3> Directions from your current location: </h3>
+        <p> INSERT GOOGLE MAP </p>
+      </div>
+      <div>
+        <h3> Parking: </h3>
+        <p>
+          Glasgow Fort has 2,500 parking spaces which are all FREE including a
+          secure multi-storey car park located next to Vue. Car parks will
+          remained open but are temporarily closed when they reach full
+          capacity.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default FindUs;
