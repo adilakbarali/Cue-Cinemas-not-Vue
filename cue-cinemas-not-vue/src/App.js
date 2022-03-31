e6cf1efc67be2bf7080024993ea19c6663d49836
adilakbarali <adilakbarali@gmail.com>
2022-03-31 11:40:18 +0100

    Added booking form, updated schemas in API to match the correct data types and field names on Atlas MongoDB

--- cue-cinemas-not-vue/src/App.js
+++ cue-cinemas-not-vue/src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './resources/logo.svg';
+import './css/App.css';
 import NavBar from './components/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import HomePage from './components/HomePage';
 import HomeSlideShow from './components/HomeSlideShow';
 import AboutUs from './components/AboutUs';
 import NewMovies from './components/NewMovies';
+import BookingForm from './components/BookingForm';
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
       <NavBar/>
       {/* <HomePage/> */}
       {/* <AboutUs/> */}
-      <NewMovies></NewMovies>
+      <BookingForm sID="62430f13e60e734b2865c365"/>
     </div>
   );
 }
---
+++ cue-cinemas-not-vue/src/components/BookingForm.jsx
@@ -0,0 +1,114 @@
+import axios from 'axios';
+import { useRef } from 'react';
+import { Form, Button } from 'react-bootstrap';
+
+const BookingForm = (sID) => {
+    
+    const post_name = useRef(null);
+    const post_seats = useRef(null);
+    const post_adults = useRef(null);
+    const post_children = useRef(null);
+    var post_concessions = [];
+
+    const appendConcession = (event) => {
+        var e = event.target;
+        if(e.checked === true){
+            post_concessions.push(e.value)
+        }
+        else{
+            for(let i=0; i < post_concessions.length; i++){
+                if(post_concessions[i] === e.value){
+                    post_concessions.splice(i, 1);
+                    break;
+                }
+            }
+        }
+    }
+
+
+    const sendData = () => {
+        var booking = {
+            "screen_id" : sID.sID,
+            "full_name" : post_name.current.value,
+            "number_of_seats" : parseInt(post_seats.current.value),
+            "number_of_adults" : parseInt(post_adults.current.value),
+            "number_of_children" : parseInt(post_children.current.value),
+            "concessions" : post_concessions
+        }
+        console.log(booking);
+        axios.post('http://localhost:4494/booking/create', booking)
+        .then((response) => {
+            console.log(response);
+        })
+    };
+
+    return ( 
+        <>
+            <Form>
+                <Form.Group className="mb-3" controlId="formFullName">
+                    <Form.Label>Full Name</Form.Label>
+                    <Form.Control type="text" placeholder="Enter full name" ref={post_name}/>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formNumberOfSeats">
+                    <Form.Label>Number Of Seats</Form.Label>
+                    <Form.Select type="select" ref={post_seats}>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                    <option>6</option>
+                    <option>7</option>
+                    <option>8</option>
+                    </Form.Select>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formNumberOfAdults">
+                    <Form.Label>Number Of Adults</Form.Label>
+                    <Form.Select type="select" ref={post_adults}>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                    <option>6</option>
+                    <option>7</option>
+                    <option>8</option>
+                    </Form.Select>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formNumberOfChildren">
+                    <Form.Label>Number Of Children</Form.Label>
+                    <Form.Select type="select" ref={post_children}>
+                    <option>0</option>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                    <option>6</option>
+                    <option>7</option>
+                    <option>8</option>
+                    </Form.Select>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formConcessions">
+                    <Form.Label>Concessions</Form.Label>
+                    <Form.Check type="checkbox" label="Salted Popcorn (Small)" value="Salted Popcorn (Small)" onChange={appendConcession}/>
+                    <Form.Check type="checkbox" label="Salted Popcorn (Large)" value="Salted Popcorn (Large)" onChange={appendConcession}/>
+                    <Form.Check type="checkbox" label="Sweet Popcorn (Small)" value="Sweet Popcorn (Small)" onChange={appendConcession}/>
+                    <Form.Check type="checkbox" label="Sweet Popcorn (Large)" value="Sweet Popcorn (Large)" onChange={appendConcession}/>
+                    <Form.Check type="checkbox" label="Mixed Popcorn (Small)" value="Mixed Popcorn (Small)" onChange={appendConcession}/>
+                    <Form.Check type="checkbox" label="Mixed Popcorn (Large)" value="Mixed Popcorn (Large)" onChange={appendConcession}/>
+                    <Form.Check type="checkbox" label="Nachos" value="Nachos" onChange={appendConcession}/>
+                    <Form.Check type="checkbox" label="Hot Dog" value="Hot Dog" onChange={appendConcession}/>
+                    <Form.Check type="checkbox" label="Soft Drink (Small)" value="Soft Drink (Small)" onChange={appendConcession}/>
+                    <Form.Check type="checkbox" label="Soft Drink (Medium)" value="Soft Drink (Medium)" onChange={appendConcession}/>
+                    <Form.Check type="checkbox" label="Soft Drink (Large)" value="Soft Drink (Large)" onChange={appendConcession}/>
+                </Form.Group>
+                <Button variant="primary" type="button" onClick={sendData}>
+                    Book!
+                </Button>
+            </Form>
+        </>
+     );
+}
+ 
+export default BookingForm;
