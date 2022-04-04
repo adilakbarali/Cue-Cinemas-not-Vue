7b0230d90f01cf248f86fb1924e6ff88e99e7fe6
adilakbarali <adilakbarali@gmail.com>
2022-04-04 12:43:01 +0100

    Added changes made in main branch. Added new Route for discussions. Added functionality to bookings page

--- cue-cinemas-not-vue/src/components/Bookings.jsx
+++ cue-cinemas-not-vue/src/components/Bookings.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useRef, useState } from 'react';
 import axios from 'axios';
-import { Form } from 'react-bootstrap';
+import { Form, Button } from 'react-bootstrap';
 import Movies from './Movies';
+import BookingForm from './BookingForm';
 
 const Bookings = (sID) => {
 
@@ -12,8 +13,10 @@ const Bookings = (sID) => {
     const [error, setError] = useState(null);
     
     const findMovie = useRef(null);
-    var outputMovie;
-    var test;
+    const [outputMovie, setOutputMovie] = useState({});
+    const [screen, setScreen] = useState();
+
+    var tempScreens = [];
 
     useEffect(() => {
         axios.get(`http://localhost:4494/movie/getAll`) // Sending a get request to the server
@@ -32,21 +35,24 @@ const Bookings = (sID) => {
     const getScreens = () => {
             axios.get(`http://localhost:4494/screen/find?movie_id=${findMovie.current.value}`) // Sending a get request to the server
               .then((response) => { 
-                setScreenData(response.data); // An array of objects stored as state              
-                getPreviewMovie();
-                
+                tempScreens = response.data;
+                tempScreens.sort(function(a, b){
+                    return new Date(b.release_date) - new Date(a.release_date)
+                })
+                setScreenData(tempScreens);
+                getPreviewMovie();     
             });
         }
 
     const getPreviewMovie = () => {
-        axios.get(`http://localhost:4494/movie/get/${findMovie.current.value}`)
-        .then((response) => { 
-            outputMovie = response.data;
-            console.log(outputMovie)
-            setLoadedScreens(true);// if there is a response, loaded = true
-            });
+            for(var i=0; i<movieData.length;i++){
+                if (movieData[i]._id === findMovie.current.value){
+                    setOutputMovie(movieData[i]);
+                    setLoadedScreens(true);
+                    break;
+                }
+            } 
         }
-
 
     return ( 
         <>
@@ -57,8 +63,15 @@ const Bookings = (sID) => {
                     return <option value={movie._id}>{movie.title}</option>;       
                 },)}
             </Form.Select>
-            {loadedScreens === true &&
+            {loadedScreens &&
             <Movies data={outputMovie}/>}
+            {loadedScreens &&
+            screenData.map((screen, key) => {
+                return <Button variant="primary" onClick={(e) => setScreen(screen._id)}>{screen.date_time}</Button>;       
+            },)}
+            {screen &&
+                <BookingForm sID={screen}/>
+            }
         </>
      );
 }
