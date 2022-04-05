e454edabd8917eb37c9190640739243fe2087ad4
Kiera-Hegarty <kiera-hegarty@hotmail.com>
2022-04-05 12:34:01 +0100

    adding get all tests

--- new-api/tests/movieTest.js
+++ new-api/tests/movieTest.js
@@ -1,6 +1,3 @@
-/* eslint-disable no-unused-expressions */
-/* eslint-disable prefer-template */
-/* eslint-disable quotes */
 const { expect } = require("chai");
 const chai = require("chai");
 const chaiHttp = require("chai-http");
@@ -10,73 +7,10 @@ const { it, describe, beforeEach } = require("mocha");
 
 const server = require("../index");
 
-const Movie = require("../db");
+const Movie = require("../");
 
 describe("Test Movie", () => {
   let testMovie;
-
-  beforeEach((done) => {
-    Movie.deleteMany((err) => {
-      if (!err) {
-        Movie.create(
-          {
-            title: "The Hunger Games",
-            description: "Book adaptation",
-            ageRating: "12A",
-          },
-          (error, created) => {
-            if (!error) {
-              testMovie = created;
-            }
-            return done();
-          },
-        );
-      }
-    });
-  });
-
-  it("Should create movie information", (done) => {
-    chai
-      .request(server)
-      .post("/movie/create")
-      .send({
-        title: "Thor: Ragnarok",
-        description: "Marvel movie",
-        ageRating: "12A",
-      })
-      .end((err, res) => {
-        expect(err).to.be.null;
-        expect(res).to.have.status(201);
-        expect(res).to.haveOwnProperty("text", "Successfully created");
-        return done();
-      });
-  });
-
-  it("Should NOT create movie information", (done) => {
-    chai
-      .request(server)
-      .post("/movie/create")
-      .send()
-      .end((err, res) => {
-        expect(err).to.be.null;
-        expect(res).to.have.status(500);
-        return done();
-      });
-  });
-
-  it("Should find movie information", (done) => {
-    chai
-      .request(server)
-      .get("/movie/get/" + testMovie.id)
-      .end((err, res) => {
-        expect(err).to.be.null;
-        expect(res).to.have.status(200);
-        expect(res.body).to.haveOwnProperty("title", "The Hunger Games");
-        expect(res.body).to.haveOwnProperty("description", "Book adaptation");
-        expect(res.body).to.haveOwnProperty("ageRating", "12A");
-        return done();
-      });
-  });
 
   it("Should find ALL movie information", (done) => {
     chai
@@ -89,28 +23,49 @@ describe("Test Movie", () => {
       });
   });
 
-  it("Should update movie information", (done) => {
-    chai
-      .request(server)
-      .put("/movie/replace/" + testMovie.id)
-      .end((err, res) => {
-        expect(err).to.be.null;
-        expect(res).to.have.status(202);
-        expect(res.body).to.haveOwnProperty("title", "The Hunger Games");
-        expect(res.body).to.haveOwnProperty("description", "Book adaptation");
-        expect(res.body).to.haveOwnProperty("ageRating", "12A");
-        return done();
-      });
-  });
+});
 
-  it("Should delete movie information", (done) => {
+describe("Test Booking", () => {
+  let testBooking;
+
+  it("Should find ALL bookings", (done) => {
     chai
-      .request(server)
-      .delete("/movie/remove/" + testMovie.id)
-      .end((err, res) => {
-        expect(err).to.be.null;
-        expect(res).to.have.status(204);
-        return done();
-      });
+    .request(server)
+    .get("/booking/getAll/")
+    .end((err, res) => {
+      expect(err).to.be.null;
+      expect(res).to.have.status(200);
+      return done();
+    });
+  });
+});
+
+describe("Test Screens", () => {
+  let testScreens;
+
+  it("Should find ALL screens", (done) => {
+    chai
+    .request(server)
+    .get("/screen/getAll/")
+    .end((err, res) => {
+      expect(err).to.be.null;
+      expect(res).to.have.status(200);
+      return done();
+    });
+  });
+});
+
+describe("Test Discussion", () => {
+  let testDiscussion;
+
+  it("Should find ALL discussions", (done) => {
+    chai
+    .request(server)
+    .get("/discussion/getAll/")
+    .end((err, res) => {
+      expect(err).to.be.null;
+      expect(res).to.have.status(200);
+      return done();
+    });
   });
 });
