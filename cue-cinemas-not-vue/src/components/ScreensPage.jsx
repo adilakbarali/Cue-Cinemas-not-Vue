21cfed52cdb44b33b959219fdd24bf853af2d1ae
Conor Kelly <conorkelly10@aol.com>
2022-04-05 18:41:30 +0100

    creating screens page

---
+++ cue-cinemas-not-vue/src/components/ScreensPage.jsx
@@ -0,0 +1,17 @@
+import DirectorsScreen from "./DirectorsScreen";
+import StandardScreen from "./StandardScreen";
+
+const ScreensPage = () => {
+    return ( 
+        <>
+        <br />
+        <br />
+        <StandardScreen/>
+        <br />
+        <br />
+        <DirectorsScreen/>
+        </>
+     );
+}
+ 
+export default ScreensPage;
